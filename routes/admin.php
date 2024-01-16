60b59634ccf92d0cf8bddec72d270dd6e26645e9
Rashed <rashedalamj@gmail.com>
2024-01-16 15:23:37 +0600

    separated routes for AdminPanel

---
+++ routes/admin.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+use App\Livewire\AdminPanel\Dashboard;
+use App\Livewire\AdminPanel\Category\Categories;
+use App\Livewire\AdminPanel\News\NewsList;
+use App\Livewire\AdminPanel\Messages\MessageList;
+use App\Livewire\AdminPanel\Writers\WriterList;
+use App\Livewire\AdminPanel\Settings\Settings;
+use App\Livewire\AdminPanel\Profile\Profile;
+use App\Livewire\AdminPanel\PrivacyPolicy\PrivacyPolicy;
+use App\Livewire\AdminPanel\TermsConditions\TermsConditions;
+
+
+
+Route::get("dashboard", Dashboard::class)->name("dashboard");
+Route::get("categories", Categories::class)->name("categories");
+Route::get("news-list", NewsList::class)->name("news-list");
+Route::get("message-list", MessageList::class)->name("message-list");
+Route::get("writer-list", WriterList::class)->name("writer-list");
+Route::get("settings", Settings::class)->name("settings");
+Route::get("profile", Profile::class)->name("profile");
+Route::get("privacy-policy", PrivacyPolicy::class)->name("privacy-policy");
+Route::get("terms-conditions", TermsConditions::class)->name("terms-conditions");
