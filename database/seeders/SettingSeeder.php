b7f5cd4038d52432a5cc05477a9c59cb0c0f2d50
Rashed <rashedalamj@gmail.com>
2024-01-19 04:10:34 +0600

    settings related migration , model & seeder added

---
+++ database/seeders/SettingSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Setting;
+use Illuminate\Database\Seeder;
+
+class SettingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $setting = new Setting();
+        $setting->logo = "logo.png";
+        $setting->favicon = "fav.png";
+        $setting->privacy_policy = "Here is Privacy Policy.";
+        $setting->terms_consitions = "Here is Terms & Conditions.";
+        $setting->save();
+    }
+}
