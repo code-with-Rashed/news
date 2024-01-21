7dbbaabbac410b6c9b3dafd0e9b819cea8a8bbd1
Rashed <rashedalamj@gmail.com>
2024-01-21 20:48:33 +0600

    bug fixed

--- public/assets/js/activeSidebar.js
+++ public/assets/js/activeSidebar.js
@@ -5,10 +5,10 @@ var activePage = () => {
   const sidebar = document.querySelectorAll(".list-group-item");
   const sidebar_link = document.querySelectorAll(".list-group-item a");
   for (let i = 0; i < sidebar_link.length; i++) {
-      let links = sidebar_link[i].href;
-      if(links == location.href){
-        sidebar[i].classList.add("list-group-item-primary");
-      }
+    let links = sidebar_link[i].href;
+    if (links == location.origin + location.pathname) {
+      sidebar[i].classList.add("list-group-item-primary");
+    }
   }
 };
 activePage();
