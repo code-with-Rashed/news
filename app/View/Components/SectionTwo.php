d5ec073536a0544757c8b0551fae145fe56a297d
Rashed <rashedalamj@gmail.com>
2024-03-01 02:40:04 +0600

    show news in second section slot

---
+++ app/View/Components/SectionTwo.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\View\Components;
+
+use App\Models\Category;
+use App\Models\News;
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class SectionTwo extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        $category = Category::all();
+        if (!is_null($category) && count($category) >= 2) {
+            $category_name = $category[1]['name'];
+            $section_two_news = News::with('category')->where('category_id', $category[1]['id'])->orderByDesc('id')->paginate(6);
+        }
+        return view('components.section-two')->with(compact('section_two_news', 'category_name'));
+    }
+}
---
+++ resources/views/components/section-two.blade.php
@@ -0,0 +1,27 @@
+<div class="container mt-5">
+    <p class="h3 mb-2 text-uppercase">{{ $category_name }}</p>
+    <hr />
+    <div class="row">
+        @foreach ($section_two_news as $news)
+            <div class="col-md-4 mb-2">
+                <div>
+                    <a href="read.html" class="text-decoration-none text-dark d-block">
+                        <img src="{{ asset('storage/media/news/' . $news->image) }}" alt="news-image"
+                            class="img-fluid rounded" height="200px" loading="lazy" />
+                    </a>
+                </div>
+                <div>
+                    <a href="read.html" class="d-block text-dark text-decoration-none mt-1">
+                        <p class="h5">{{ $news->title }}</p>
+                    </a>
+                </div>
+                <div>
+                    <p>
+                        {!! substr($news->news, 0, 800) !!}
+                        <a href="read.html" class="text-primary text-decoration-none">...Read more</a>
+                    </p>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
