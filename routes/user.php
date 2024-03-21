4a3affb2feae5e457952159d5e7436eb4f75c79f
Rashed <rashedalamj@gmail.com>
2024-03-21 12:47:47 +0600

    user routes update

--- routes/user.php
+++ routes/user.php
@@ -9,12 +9,15 @@ use App\Livewire\UsersPanel\LikedNews;
 use App\Livewire\UsersPanel\InterestedCategory;
 use App\Livewire\UsersPanel\Profile;
 
-Route::get("dashboard", Dashboard::class)->name("dashboard");
-Route::get("news/feed", NewsFeed::class)->name("news-feed");
-Route::get("bookmarked/news", BookmarkedNews::class)->name("bookmarked-news");
-Route::get("liked/news", LikedNews::class)->name("liked-news");
-Route::get("interested/category", InterestedCategory::class)->name("interested-category");
-Route::get("profile", Profile::class)->name("profile");
-Route::get("logout", function () {
-    //
-})->name("logout");
+Route::middleware("access_user_pages")->group(function () {
+    Route::get("dashboard", Dashboard::class)->name("dashboard");
+    Route::get("news/feed", NewsFeed::class)->name("news-feed");
+    Route::get("bookmarked/news", BookmarkedNews::class)->name("bookmarked-news");
+    Route::get("liked/news", LikedNews::class)->name("liked-news");
+    Route::get("interested/category", InterestedCategory::class)->name("interested-category");
+    Route::get("profile", Profile::class)->name("profile");
+    Route::get("logout", function () {
+        session()->forget("user");
+        return redirect()->route("home-page");
+    })->name("logout");
+});
