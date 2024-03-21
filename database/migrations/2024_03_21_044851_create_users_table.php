c57cd422813433d653a67456db66e2b49f2da78a
Rashed <rashedalamj@gmail.com>
2024-03-21 11:00:51 +0600

    create user migration & model

---
+++ database/migrations/2024_03_21_044851_create_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',50);
+            $table->string('email',60);
+            $table->string('password',50);
+            $table->enum('status',[1,0])->default(1)->comment('status 1 means user is active & 0 means user is inactive');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
