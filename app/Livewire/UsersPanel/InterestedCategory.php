9a3a8680371105871bcbec6f5fd2c59f5a214364
Rashed <rashedalamj@gmail.com>
2024-03-26 02:09:44 +0600

    manage interest news category for users

--- app/Livewire/UsersPanel/InterestedCategory.php
+++ app/Livewire/UsersPanel/InterestedCategory.php
@@ -2,6 +2,8 @@
 
 namespace App\Livewire\UsersPanel;
 
+use App\Models\Category;
+use App\Models\InterestedCategory as ModelsInterestedCategory;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Title;
 use Livewire\Component;
@@ -10,8 +12,42 @@ use Livewire\Component;
 #[Title("Interested on Categories")]
 class InterestedCategory extends Component
 {
+    public $selected_category = [];
+    public $user_id;
+
+    public function mount()
+    {
+        $this->user_id = session()->get('user')['id'];
+    }
+
+    public function chose($category_id)
+    {
+        if (!in_array($category_id, $this->selected_category)) {
+            array_push($this->selected_category, $category_id);
+        } else {
+            $key = array_search($category_id, $this->selected_category);
+            ModelsInterestedCategory::where('user_id', $this->user_id)->where('category_id', $this->selected_category[$key])->delete();
+            unset($this->selected_category[$key]);
+        }
+    }
+
+    public function save()
+    {
+
+        if (!empty($this->selected_category)) {
+            foreach ($this->selected_category as  $value) {
+                ModelsInterestedCategory::updateOrCreate([
+                    "user_id" => $this->user_id,
+                    "category_id" => $value
+                ]);
+            }
+        }
+        $this->redirectRoute('user.interested-category');
+    }
     public function render()
     {
-        return view('livewire.users-panel.interested-category');
+        $categories = Category::all();
+        $interested_category = ModelsInterestedCategory::where('user_id', $this->user_id)->pluck('category_id');
+        return view('livewire.users-panel.interested-category')->with(compact('categories', 'interested_category'));
     }
 }
--- resources/views/livewire/users-panel/interested-category.blade.php
+++ resources/views/livewire/users-panel/interested-category.blade.php
@@ -1,23 +1,13 @@
 <div class="row shadow-sm rounded py-3 px-2 mb-3 m-auto">
     <h3 class="mb-2">My Interested Category</h3>
-    <div class="form-check form-switch mb-1">
-        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" checked>
-        <label class="form-check-label" for="flexSwitchCheckDefault">International</label>
-    </div>
-    <div class="form-check form-switch mb-1">
-        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
-        <label class="form-check-label" for="flexSwitchCheckDefault">Politics</label>
-    </div>
-    <div class="form-check form-switch mb-1">
-        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" checked>
-        <label class="form-check-label" for="flexSwitchCheckDefault">Lifestyle</label>
-    </div>
-    <div class="form-check form-switch mb-1">
-        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
-        <label class="form-check-label" for="flexSwitchCheckDefault">Sports</label>
-    </div>
-    <div class="form-check form-switch mb-1">
-        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" checked>
-        <label class="form-check-label" for="flexSwitchCheckDefault">Travel</label>
-    </div>
+    @foreach ($categories as $category)
+        <div class="form-check form-switch mb-1" wire:key="{{ $category->id }}">
+            <input type="checkbox" class="form-check-input" role="switch" id="select-category-{{ $category->id }}"
+                wire:click="chose({{ $category->id }})"
+                @if ($interested_category->contains($category->id)) @checked(true)
+            wire:init="chose({{ $category->id }})" @endif>
+            <label class="form-check-label" for="select-category-{{ $category->id }}">{{ $category->name }}</label>
+        </div>
+    @endforeach
+    <button type="button" class="btn btn-primary my-2 w-25" wire:click.debounce.100ms="save()">Save</button>
 </div>
