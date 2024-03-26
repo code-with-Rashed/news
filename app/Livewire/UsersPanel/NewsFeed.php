fa8daec728c03969053543a74914296a1d2e2378
Rashed <rashedalamj@gmail.com>
2024-03-26 15:27:02 +0600

    manage news feed

--- app/Livewire/UsersPanel/NewsFeed.php
+++ app/Livewire/UsersPanel/NewsFeed.php
@@ -2,6 +2,8 @@
 
 namespace App\Livewire\UsersPanel;
 
+use App\Models\InterestedCategory;
+use App\Models\News;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Title;
 use Livewire\Component;
@@ -10,8 +12,18 @@ use Livewire\Component;
 #[Title("News Feed")]
 class NewsFeed extends Component
 {
+    public function logedin_user_id()
+    {
+        return session()->get('user')['id'];
+    }
     public function render()
     {
-        return view('livewire.users-panel.news-feed');
+        $news_feed = [];
+        $category = InterestedCategory::where('user_id', $this->logedin_user_id())->pluck('category_id');
+        foreach ($category as $value) {
+            $news = News::with('category')->where('category_id', $value)->get()->last();
+            array_push($news_feed,$news->toArray());
+        }
+        return view('livewire.users-panel.news-feed', compact('news_feed'));
     }
 }
