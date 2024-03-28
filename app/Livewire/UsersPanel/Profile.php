a8d874e0390a00e920a707ce8b7ecd0ee3205eb2
Rashed <rashedalamj@gmail.com>
2024-03-28 11:21:04 +0600

    manage user profile

--- app/Livewire/UsersPanel/Profile.php
+++ app/Livewire/UsersPanel/Profile.php
@@ -2,16 +2,104 @@
 
 namespace App\Livewire\UsersPanel;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Title;
 use Livewire\Component;
+use Livewire\Features\SupportFileUploads\WithFileUploads;
 
 #[Layout("UsersPanel.index")]
 #[Title("Profile")]
 class Profile extends Component
 {
+    use WithFileUploads;
+    public $old_photo, $photo, $name, $email;
+    public $old_password, $password, $password_confirmation;
+
+    public function logedin_user_id()
+    {
+        return session()->get('user')['id'];
+    }
+
+    public function update_user()
+    {
+        $id = $this->logedin_user_id();
+        $this->validate([
+            "photo" => "nullable|image|mimes:png,jpg,jpeg,webp",
+            "name" => "required|string|max:100",
+            "email" => "required|email|max:100|unique:users,email,$id",
+        ]);
+
+        $user = User::find($id);
+        $user->name = $this->name;
+        $user->email = $this->email;
+
+        if ($this->photo) {
+            $image_name = $this->photo->store("public/media/profile");
+            $image = basename($image_name);
+            $user->photo = $image;
+
+            if ($this->old_photo != "user.jpg") {
+                if (Storage::exists("public/media/profile/" . $this->old_photo)) {
+                    Storage::delete("public/media/profile/" . $this->old_photo);
+                }
+            }
+        }
+
+        $user->update();
+
+        $this->reset(['old_photo', 'photo', 'name', 'email']);
+
+        $this->dispatch(
+            "alert",
+            type: "success",
+            title: "Profile information successfuly updated."
+        );
+    }
+
+    public function change_password()
+    {
+        $this->validate([
+            "old_password" => "required|max:100",
+            "password" => "required|min:5|max:100|confirmed",
+            "password_confirmation" => "required|min:5|max:100",
+        ]);
+
+        $id = $this->logedin_user_id();
+        $user = User::find($id);
+
+        if (Hash::check($this->old_password, $user->password)) {
+
+            $user->password = Hash::make($this->password);
+            $user->update();
+
+            $this->dispatch(
+                "alert",
+                type: "success",
+                title: "Password is updated."
+            );
+        } else {
+            $this->dispatch(
+                "alert",
+                type: "error",
+                title: "Old Password is Wrong!!"
+            );
+        }
+
+        $this->reset(['old_password', 'password', 'password_confirmation']);
+    }
+
     public function render()
     {
+        $id = $this->logedin_user_id();
+        $user = User::find($id);
+
+        $this->old_photo = $user->photo;
+        $this->name = $user->name;
+        $this->email = $user->email;
+
         return view('livewire.users-panel.profile');
     }
 }
--- resources/views/livewire/users-panel/profile.blade.php
+++ resources/views/livewire/users-panel/profile.blade.php
@@ -1,59 +1,98 @@
-<div class="row ms-1">
-    <div class="col mt-5 bg-white shadow-sm p-4 rounded">
-        <h2 class="fw-bold mb-3">Profile Management</h2>
-        <div class="row">
-            <!-- Profile Update Area Start -->
-            <div class="col-md-6">
-                <form class="shadow rounded p-4">
-                    <div class="mb-4">
-                        <img src="../image/user.jpg" alt="Profile-Photo" class="rounded" width="100px" height="130px"
-                            style="object-fit: cover">
-                    </div>
-                    <div class="form-group mb-3">
-                        <label for="photo" class="mb-1">Change Photo</label>
-                        <input type="file" id="photo" class="form-control" accept=".png,.jpeg,.jpg">
-                    </div>
-                    <div class="form-group mb-3">
-                        <label for="name" class="mb-1">Name</label>
-                        <input type="text" id="name" class="form-control" value="Rashed alam" required=""
-                            maxlength="30">
-                    </div>
-                    <div class="form-group mb-3">
-                        <label for="email" class="mb-1">Email</label>
-                        <input type="email" id="email" class="form-control" value="example@mail.com"
-                            required="" maxlength="50">
-                    </div>
-                    <button type="submit" class="btn btn-primary">
-                        Update
-                    </button>
-                </form>
+<div>
+    <div class="row ms-1">
+        <div class="col mt-5 bg-white shadow-sm p-4 rounded">
+            <h2 class="fw-bold mb-3">Profile Management</h2>
+            <div class="row">
+                <!-- Profile Update Area Start -->
+                <div class="col-md-6">
+                    <form class="shadow rounded p-4" wire:submit="update_user()">
+                        @if ($photo)
+                            <div class="mb-4">
+                                <img src="{{ $photo->temporaryUrl() }}" alt="Profile-Photo" class="rounded-circle"
+                                    width="100px" height="100px" />
+                            </div>
+                        @else
+                            <div class="mb-4">
+                                <img src="{{ asset("storage/media/profile/$old_photo") }}" alt="Profile-Photo"
+                                    class="rounded-circle" width="100px" height="100px" />
+                            </div>
+                        @endif
+                        <div class="form-group mb-3">
+                            <label for="photo" class="mb-1">Change Photo</label>
+                            <input type="file" id="photo" class="form-control" accept="image/*"
+                                wire:model="photo" />
+                            @error('photo')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                            <div wire:loading wire:tergate="photo">
+                                <span class="text-primary">Uploading...</span>
+                            </div>
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="name" class="mb-1">Name</label>
+                            <input type="text" id="name" class="form-control" maxlength="100"
+                                wire:model="name" />
+                            @error('name')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="email" class="mb-1">Email</label>
+                            <input type="email" id="email" class="form-control" maxlength="100"
+                                wire:model="email" />
+                            @error('email')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <button type="submit" class="btn btn-primary">
+                            <span class="spinner-border spinner-border-sm" aria-hidden="true" wire:loading
+                                wire:tergate="update_user"></span>
+                            Update
+                        </button>
+                    </form>
+                </div>
+                <!-- Profile Update Area End -->
+                <!-- Change Password Area Start -->
+                <div class="col-md-6">
+                    <form class="shadow rounded p-4" wire:submit="change_password()">
+                        <p class="h5 fw-bold mb-3">Update Password</p>
+                        <div class="form-group mb-3">
+                            <label for="oldPassword" class="mb-1">Old Password</label>
+                            <input type="password" wire:model="old_password" id="oldPassword" class="form-control"
+                                placeholder="Enter old password" maxlength="100" required />
+                            @error('old_password')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="newPassword" class="mb-1">New Password</label>
+                            <input type="password" wire:model="password" id="newPassword" class="form-control"
+                                placeholder="Enter new password" maxlength="100" required />
+                            @error('password')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="confirmPassword" class="mb-1">Confirm Password</label>
+                            <input type="password" wire:model="password_confirmation" id="confirmPassword"
+                                class="form-control" placeholder="Enter confirm password" maxlength="100" required />
+                            @error('password_confirmation')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <button type="submit" class="btn btn-primary">
+                            <span class="spinner-border spinner-border-sm" aria-hidden="true" wire:loading
+                                wire:tergate="change_password"></span>
+                            Change Password
+                        </button>
+                    </form>
+                </div>
+                <!-- Change Password Area End -->
             </div>
-            <!-- Profile Update Area End -->
-            <!-- Change Password Area Start -->
-            <div class="col-md-6">
-                <form class="shadow rounded p-4">
-                    <p class="h5 fw-bold mb-3">Update Password</p>
-                    <div class="form-group mb-3">
-                        <label for="oldPassword" class="mb-1">Old Password</label>
-                        <input type="password" id="oldPassword" class="form-control" placeholder="Enter old password"
-                            maxlength="25" required="">
-                    </div>
-                    <div class="form-group mb-3">
-                        <label for="newPassword" class="mb-1">New Password</label>
-                        <input type="password" id="newPassword" class="form-control" placeholder="Enter new password"
-                            maxlength="25" required="">
-                    </div>
-                    <div class="form-group mb-3">
-                        <label for="confirmPassword" class="mb-1">Confirm Password</label>
-                        <input type="password" id="confirmPassword" class="form-control"
-                            placeholder="Enter confirm password" maxlength="25" required="">
-                    </div>
-                    <button type="submit" class="btn btn-primary">
-                        Change Password
-                    </button>
-                </form>
-            </div>
-            <!-- Change Password Area End -->
         </div>
     </div>
 </div>
+@push('sweetAlert')
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="{{ asset('assets/js/sweetAlert.js') }}"></script>
+@endpush
