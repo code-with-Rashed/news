f26e3fd56f6311ecfc9dcb026a928c11a08b7714
Rashed <rashedalamj@gmail.com>
2024-03-28 10:25:48 +0600

    show user related dashboard summary

--- app/Livewire/UsersPanel/Dashboard.php
+++ app/Livewire/UsersPanel/Dashboard.php
@@ -2,6 +2,10 @@
 
 namespace App\Livewire\UsersPanel;
 
+use App\Models\Bookmark;
+use App\Models\Dislike;
+use App\Models\InterestedCategory;
+use App\Models\Like;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Title;
 use Livewire\Component;
@@ -10,8 +14,18 @@ use Livewire\Component;
 #[Title("Dashboard")]
 class Dashboard extends Component
 {
+    public function logedin_user_id()
+    {
+        return session()->get('user')['id'];
+    }
+    
     public function render()
     {
-        return view('livewire.users-panel.dashboard');
+        $bookmark_news = Bookmark::where('user_id', $this->logedin_user_id())->count();
+        $disliked_news = Dislike::where('users_id', $this->logedin_user_id())->count();
+        $interested_category = InterestedCategory::where('user_id', $this->logedin_user_id())->count();
+        $liked_news = Like::where('users_id', $this->logedin_user_id())->count();
+
+        return view('livewire.users-panel.dashboard')->with(compact('bookmark_news', 'disliked_news', 'interested_category', 'liked_news'));
     }
 }
--- resources/views/livewire/users-panel/dashboard.blade.php
+++ resources/views/livewire/users-panel/dashboard.blade.php
@@ -1,14 +1,18 @@
-<div class="row ms-1 justify-content-around mt-5 text-center">
-    <div class="col-md-3 bg-white shadow-sm p-4 rounded mb-2">
+<div class="row ms-1 justify-content-start mt-5 text-center">
+    <div class="col-md-3 bg-white shadow-sm p-4 rounded m-2">
         <strong class="d-block">INTERESTED ON CATEGORY</strong>
-        <span>5</span>
+        <span>{{ $interested_category }}</span>
     </div>
-    <div class="col-md-3 bg-white shadow-sm p-4 rounded mb-2">
-        <strong class="d-block">LIKED NEWS</strong>
-        <span>15</span>
-    </div>
-    <div class="col-md-3 bg-white shadow-sm p-4 rounded mb-2">
+    <div class="col-md-3 bg-white shadow-sm p-4 rounded m-2">
         <strong class="d-block">BOOKMARKED NEWS</strong>
-        <span>18</span>
+        <span>{{ $bookmark_news }}</span>
+    </div>
+    <div class="col-md-3 bg-white shadow-sm p-4 rounded m-2">
+        <strong class="d-block">LIKED NEWS</strong>
+        <span>{{ $liked_news }}</span>
+    </div>
+    <div class="col-md-3 bg-white shadow-sm p-4 rounded m-2">
+        <strong class="d-block">DISLIKED NEWS</strong>
+        <span>{{ $disliked_news }}</span>
     </div>
 </div>
